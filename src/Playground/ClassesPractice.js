ba59b02e8cf881eb31d2226d988d72c0cf93035e
Parasar Das <parasarkdas@gmail.com>
2018-02-05 16:51:58 +1100

    Added some comments

--- src/Playground/ClassesPractice.js
+++ src/Playground/ClassesPractice.js
@@ -68,3 +68,5 @@ class Traveller extends Student {
 }
 
 export default Person;
+
+// Just a comment
--- src/Playground/IndecisionApp.js
+++ src/Playground/IndecisionApp.js
@@ -87,3 +87,6 @@ class IndecisionApp extends Component {
 }
 
 export default IndecisionApp;
+
+
+// Just a comment
